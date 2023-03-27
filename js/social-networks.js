6bf82ad6c45816be640454a64854040ffbee0207
mikiribasmas <miquel.ribas21@gmail.com>
2023-03-27 07:28:59 +0200

    making the hamburguer

--- js/social-networks.js
+++ js/social-networks.js
@@ -1,12 +1,41 @@
+// export default (() => {
+//     const hamburguerIcons = document.querySelectorAll('.hamburguer-icon');
+//     const socialIcons = document.querySelectorAll('.menu-items ul li');
+
+//     hamburguerIcons.forEach(hamburguerIcon => {
+//         hamburguerIcon.addEventListener('click', () => {
+//             console.log('hamburguer clicked');    
+//         });
+//     });
+//     console.log('hola');
+
+// })();
+
+
+// export default (() => {
+//     const hamburguerIcons = document.querySelectorAll('.hamburguer-icon img');
+//     const menuItems = document.querySelector('.menu-items ul li');
+  
+//     hamburguerIcons.forEach(hamburguerIcon => {
+//       hamburguerIcon.addEventListener('click', () => {
+//         menuItems.classList.toggle('active');
+//       });
+//     });
+//   })();
+
 export default (() => {
-    const hamburguerIcons = document.querySelectorAll('.hamburguer-icon');
-    const socialIcons = document.querySelectorAll('.menu-items ul li');
+    const hamburguerIcons = document.querySelectorAll('.hamburguer-icon img');
+    const menuItems = document.querySelector('.menu-items ul li');
 
     hamburguerIcons.forEach(hamburguerIcon => {
-        hamburguerIcon.addEventListener('click', () => {
-            console.log('hamburguer clicked');    
-        });
+      hamburguerIcon.addEventListener('click', () => {
+        menuItems.classList.toggle('active');
+        console.log('Hamburguesa clickeada');
+        
+      });
     });
-    console.log('hola');
+  })();
 
-})();
+  
+  
+  
