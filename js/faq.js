721d94a7a7160ef66997feabeeb52ec6b8463ac1
mikiribasmas <miquel.ribas21@gmail.com>
2023-04-01 00:45:27 +0200

    feat: completed front-end layout of flipcard

--- js/faq.js
+++ js/faq.js
@@ -3,12 +3,14 @@ export default (() => {
     const dropdowns = document.querySelectorAll('.faq-content-item-title svg');
     const dropdownDescriptions = document.querySelectorAll('.faq-content-item-description p');
     const items = document.querySelectorAll('.faq-content-item');
+    const descriptions = document.querySelectorAll('.faq-content-item-description');
   
     for (let i = 0; i < dropdowns.length; i++) {
       dropdowns[i].addEventListener('click', () => {
         dropdowns[i].classList.toggle('active');
         dropdownDescriptions[i].classList.toggle('active');
         items[i].classList.toggle('active');
+        descriptions[i].classList.toggle('active');
       });
     }
   })();
--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,43 @@
 export default (() => {
     const buyNowButtons = document.querySelectorAll('.modal-button');
     const modalContainer = document.querySelector('.modal-content');
-    const closeButton = document.querySelector('.close-button');
-  
+    const closeButtons = document.querySelectorAll('.close-button');
+    const carousel = document.querySelectorAll('.modal-image-slide');
+    const modalCheckouts = document.querySelectorAll('.modal-checkout');
+    const modalForms = document.querySelectorAll('.modal-description');
 
-   
+
+
     buyNowButtons.forEach(buyNowButton => buyNowButton.addEventListener('click', () => {
-         modalContainer.style.display = 'block';
+        modalContainer.style.display = 'flex';
     }));
 
-    closeButton.addEventListener('click', () => {
-        modalContainer.style.display = 'none';
-      });
-         
+    closeButtons.forEach(closeButton => {
+        closeButton.addEventListener('click', () => {
+            modalContainer.style.display = 'none';
+        });
+    })
 
-  })();
-  
+
+    carousel.forEach(carousel => {
+        carousel.addEventListener('click', () => {
+            carousel.classList.toggle('slide-active');
+        });
+
+    })
+
+    modalCheckouts.forEach(modalCheckout => {
+        modalCheckout.addEventListener('click', () => {
+            modalForms.forEach(modalForm => {
+                modalForm.classList.toggle('activeForm');
+
+            });
+        });
+
+    })
+
+
+})();
+
+
+
